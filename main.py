18a81e9e7fc3fe9ff2a26ba738ec418f627c5988
Exllated <musicmany222@gmail.com>
2022-10-15 20:45:29 +0200

    Now custom starting link can be specified as the first argument.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit('Provide link with scheme (e.g. \'https://example.com/\')')
+
+    if not sys.argv[1].startswith('http://') and not sys.argv[1].startswith('https://'):
+        sys.exit('Provide link with scheme (e.g. \'https://example.com/\')')
+
     import configs
     configs.load()
 
     import scheduler
-    scheduler.run('https://google.com/')
+    scheduler.run(sys.argv[1])
     pass
